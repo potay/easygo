5039452eca776ee2df5366853af88402aeb6deee
Paul Chun <me@paulchun.com>
2015-07-05 14:44:53 -0700

    Working product! v0.2

--- popup.js
+++ popup.js
@@ -5,32 +5,64 @@ var salert = function(thing) {
   //alert(thing);
 }
 
-function updatePage() {
+function updatePage(pairDict, type) {
   var entriesTable = document.getElementById('entries');
-  var currentEntries = entriesTable.children
+  var currentEntries = entriesTable.children;
 
-  while(currentEntries.length > 0) {
-    entriesTable.removeChild(currentEntries[0]);
-  }
+  console.log(pairDict);
 
-  chrome.storage.sync.get({'mapping': {}},
-    function(cache) {
-      for (pair in cache.mapping){
-        var entryRow = entriesTable.insertRow(0);
-        entryRow.id = pair;
-        var entryFromCell = entryRow.insertCell(0);
-        var entryToCell = entryRow.insertCell(1);
-        var entryDeleteCell = entryRow.insertCell(2);
-        entryFromCell.innerHTML = "http://"+pair+"/";
-        entryToCell.innerHTML = "http://"+cache.mapping[pair]+"/";
-        entryDeleteCell.innerHTML = "<a class=\"btn-floating waves-effect waves-light red deleteEntry\" data-from=\""+pair+"\"><i class=\"material-icons\">delete</i></a>";
+  if (pairDict == undefined) {
+
+    while(currentEntries.length > 0) {
+      entriesTable.removeChild(currentEntries[0]);
+    }
+
+    chrome.storage.sync.get({'mapping': {}},
+      function(cache) {
+        var pairs = Object.keys(cache.mapping).sort()
+        console.log(pairs);
+        for (i in pairs){
+          var pair = pairs[i];
+          var entryRow = entriesTable.insertRow(-1);
+          entryRow.id = "entry_"+pair;
+          var entryFromCell = entryRow.insertCell(0);
+          var entryToCell = entryRow.insertCell(1);
+          var entryDeleteCell = entryRow.insertCell(2);
+          entryFromCell.innerHTML = "http://"+pair+"/";
+          entryToCell.innerHTML = "http://"+cache.mapping[pair]+"/";
+          entryDeleteCell.innerHTML = "<a class=\"btn-floating waves-effect waves-light red deleteEntry\" data-from=\""+pair+"\"><i class=\"material-icons\">delete</i></a>";
+        }
+        deleteButtons = document.getElementsByClassName('deleteEntry');
+        for (i = 0; i < deleteButtons.length; i++) {
+          deleteButtons[i].addEventListener('click', generateDeleteEntryButton(deleteButtons[i].dataset.from));
+        }
       }
+    );
+  } else {
+    console.log(type);
+    if (type == "delete") {
+      var childEntry = document.getElementById('entry_'+pairDict.from);
+      entriesTable.removeChild(childEntry);
+    } else if (type == "add") {
+      if (pairDict.repeat) {
+        var childEntry = document.getElementById('entry_'+pairDict.from);
+        entriesTable.removeChild(childEntry);
+      }
+      var entryRow = entriesTable.insertRow(pairDict.i);
+      entryRow.id = "entry_"+pairDict.from;
+      var entryFromCell = entryRow.insertCell(0);
+      var entryToCell = entryRow.insertCell(1);
+      var entryDeleteCell = entryRow.insertCell(2);
+      entryFromCell.innerHTML = "http://"+pairDict.from+"/";
+      entryToCell.innerHTML = "http://"+pairDict.to+"/";
+      entryDeleteCell.innerHTML = "<a class=\"btn-floating waves-effect waves-light red deleteEntry\" data-from=\""+pairDict.from+"\"><i class=\"material-icons\">delete</i></a>";
+
       deleteButtons = document.getElementsByClassName('deleteEntry');
       for (i = 0; i < deleteButtons.length; i++) {
         deleteButtons[i].addEventListener('click', generateDeleteEntryButton(deleteButtons[i].dataset.from));
       }
     }
-  );
+  }
 }
 
 function saveChanges() {
@@ -48,10 +80,18 @@ function saveChanges() {
   chrome.storage.sync.get({'mapping': {}},
     function(cache) {
       var newMapping = cache.mapping;
-      newMapping[theFromValue] = theToValue;
+      if (theFromValue in newMapping) {
+        var repeat = true;
+        newMapping[theFromValue] = theToValue;
+        var i = Object.keys(newMapping).sort().indexOf(theFromValue);
+      } else {
+        var repeat = false;
+        newMapping[theFromValue] = theToValue;
+        var i = Object.keys(newMapping).sort().indexOf(theFromValue);
+      }
       chrome.storage.sync.set({'mapping': newMapping},
         function() {
-          updatePage();
+          updatePage({'from': theFromValue, 'to': theToValue, 'i': i, 'repeat': repeat}, "add");
           return;
         }
       );
@@ -64,10 +104,11 @@ function deleteEntry(from) {
   chrome.storage.sync.get({'mapping': {}},
     function(cache) {
       var newMapping = cache.mapping;
+      var to = cache.mapping[from];
       delete newMapping[from];
       chrome.storage.sync.set({'mapping': newMapping},
         function() {
-          updatePage();
+          updatePage({'from': from, 'to': to}, "delete");
           return;
         }
       );
